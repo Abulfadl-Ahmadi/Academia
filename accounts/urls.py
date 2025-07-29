4c5707fbab7f0f904b82b23cb9dda231a2f0ccdc
Abulfadl Ahmadi <abulfadl.ahmadi@gmail.com>
2025-07-29 23:13:01 +0330

    refactor: simplify imports in urls.py add: login, token refresh and logout endpoints

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,14 @@
 # urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserProfileViewSet
+from .views import *
 
 router = DefaultRouter()
 router.register(r'profiles', UserProfileViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
+    path('api/login/', LoginView.as_view()),
+    path('api/token/refresh/', RefreshTokenView.as_view()),
+    path('api/logout/', LogoutView.as_view()),
 ]
