ed03b7052719b762d77a592af1a66938d745b73c
Abulfadl Ahmadi <abulfadl.ahmadi@gmail.com>
2025-07-29 23:10:54 +0330

    refactor: update UserManager to inherit from BaseUserManager and improve user creation methods

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import BaseUserManager
 from django.core.validators import RegexValidator
 from .validators import validate_iranian_national_id
 
 
-class UserManager(models.Manager):
+class UserManager(BaseUserManager):
+    def create_user(self, username, password=None, **extra_fields):
+        if not username:
+            raise ValueError("Username is required")
+        user = self.model(username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(username, password, **extra_fields)
+
+    def get_by_natural_key(self, username):
+        return self.get(username=username)
+    
     def students(self):
         return self.filter(role='student')
 
