49d686023b3c907b42411542437fbcfb30d3c124
Abulfadl Ahmadi <abulfadl.ahmadi@gmail.com>
2025-07-29 22:29:00 +0330

    add serializers and URLs for user profile management

---
+++ accounts/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from .models import UserProfile
+
+User = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'role', 'first_name', 'last_name']
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = UserProfile
+        fields = ['user', 'national_id', 'phone_number', 'birth_date', 'grade']
+
+    def create(self, validated_data):
+        user_data = validated_data.pop('user')
+        user = User.objects.create(**user_data)
+        profile = UserProfile.objects.create(user=user, **validated_data)
+        return profile
+
+    def update(self, instance, validated_data):
+        user_data = validated_data.pop('user', {})
+        user = instance.user
+        for attr, value in user_data.items():
+            setattr(user, attr, value)
+        user.save()
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+
+        return instance
+
---
+++ accounts/urls.py
@@ -0,0 +1,11 @@
+# urls.py
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import UserProfileViewSet
+
+router = DefaultRouter()
+router.register(r'profiles', UserProfileViewSet)
+
+urlpatterns = [
+    path('api/', include(router.urls)),
+]
