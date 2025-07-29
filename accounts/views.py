cd70f8b6c803ed1831f290f47e12a3aa62234b65
Abulfadl Ahmadi <abulfadl.ahmadi@gmail.com>
2025-07-29 22:27:49 +0330

    add UserProfileViewSet for user profile management

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import UserProfile
+from .serializers import UserProfileSerializer
 
-# Create your views here.
+class UserProfileViewSet(viewsets.ModelViewSet):
+    queryset = UserProfile.objects.select_related('user').all()
+    serializer_class = UserProfileSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.role == 'admin':
+            return self.queryset
+        return self.queryset.filter(user=user)
